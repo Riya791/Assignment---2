3ecfb04705912552a74af435a75aebd64e8bf52d
Riya791 <92538522+Riya791@users.noreply.github.com>
2023-05-15 19:40:14 +0530

    Add files via upload

--- scripts/utility/mongo_utility.py
+++ scripts/utility/mongo_utility.py
@@ -1,16 +1,17 @@
-from pymongo import MongoClient  # import mongo client to connect
-# Creating instance of mongo client
+from pymongo import MongoClient
+
+
 client = MongoClient("mongodb://intern_23:intern%40123@192.168.0.220:2717/interns_b2_23")
-# Creating database
+
+
 db = client.interns_b2_23
-inventory_item = {"id": "2210",
-                  "name": "Chikni",
-                  "quantity": "50",
-                  "cost": "8000"
-                  }
-# # Creating document
-inventory = db.riya_inventory
-# # Inserting data
-inventory.insert_one(inventory_item)
-# # Fetching data
-print(inventory.find_one({"id": "2209"}))
+Item = {
+        "id": int,
+        "name": str,
+        "quantity": int,
+        "cost": int
+}
+
+billing = db.riya_billing
+billing.insert_one(Item)
+
